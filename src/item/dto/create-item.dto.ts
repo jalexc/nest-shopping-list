36761651671398fc10c8416fbb676f7b9c655b39
Alex Carneiro <alex.carneiro@tempest.com.br>
2021-04-21 23:09:49 -0300

    create dto

--- src/item/dto/create-item.dto.ts
+++ src/item/dto/create-item.dto.ts
@@ -1,1 +1,15 @@
-export class CreateItemDto {}
+import { IsInt, IsNotEmpty, IsOptional, IsString, Min } from 'class-validator';
+
+export class CreateItemDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsOptional()
+  @IsString()
+  description: string;
+
+  @IsInt()
+  @Min(0)
+  quantity: number;
+}
